c570f97f7273a014ed7d05dabc91ff19a4fca22b
SAStevens <118325820+SAStevens@users.noreply.github.com>
2023-01-26 21:02:37 -0700

    107 Assingment 1

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import './footer.css';
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <label>Scott Stevens</label>
+        </div>
+    );
+}
+
+export default Footer; 
---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import "./navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+        menu goes here
+    </div>
+  );
+}
+
+export default Navbar;
